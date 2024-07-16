5430e956d9e7ce2e2239d61e95216accb3b5e27b
CrazyWolf13 <git@tobiasmeier.dev>
2024-07-16 10:38:02 +0200

    fix order

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -57,9 +57,9 @@ if (Test-Path -Path $xConfigPath) {
         exit
     }
     . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/$githubUser/dotfiles/main/pwsh/installer.ps1" -UseBasicParsing).Content
-    Initialize-DevEnv
     Test-Pwsh 
     Test-CreateProfile
+    Initialize-DevEnv
     Install-Config
     . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/$githubUser/dotfiles/main/pwsh/pwsh_helper.ps1" -UseBasicParsing).Content
     $null = Show-MessageBox $infoMessage 'Important Notice' -Buttons OK -Icon Information
