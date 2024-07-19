0c88bba0c91323311c4a15e0ad7c9e7c0a141413
CrazyWolf13 <git@tobiasmeier.dev>
2024-07-19 15:44:58 +0200

    small visual update

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -50,7 +50,7 @@ function Run-UpdatePowershell {
 function DownloadFile($filename) {
     $url = "https://raw.githubusercontent.com/$githubUser/dotfiles/main/pwsh/$filename"
     Invoke-WebRequest -Uri $url -OutFile "$baseDir\$filename"
-    Write-Host "File downloaded: $baseDir\$filename"
+    Write-Host "File downloaded: $baseDir\$filename" -ForegroundColor Cyan
 }
 
 # Function for checking and updating files
@@ -60,7 +60,7 @@ function CheckAndUpdateFile($filename) {
     $remoteFileContent = Invoke-WebRequest -Uri $url | Select-Object -ExpandProperty Content
 
     if ($localFileContent -ne $remoteFileContent) {
-        Write-Host "Updating file: $filename"
+        Write-Host "Updating file: $filename" -ForegroundColor DarkCyan
         DownloadFile "$filename"
         $global:updatedFilesCount += 1
     }
