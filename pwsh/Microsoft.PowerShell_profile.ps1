ca5113d98cfa3cdbe073b15651e518dfb8935b2d
Tobias <git@tobiasmeier.dev>
2024-09-03 21:05:27 +0200

    remove sudo workaround, as it's now native in 24H2

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # Check internet access
 # Use wmi as there is no timeout in pwsh  5.0 and generally slow.
 $timeout = 1000 
-$pingResult = $pingResult = Get-CimInstance -ClassName Win32_PingStatus -Filter "Address = 'github.com' AND Timeout = $timeout" -Property StatusCode
+$pingResult = Get-CimInstance -ClassName Win32_PingStatus -Filter "Address = 'github.com' AND Timeout = $timeout" -Property StatusCode 2>$null
 if ($pingResult.StatusCode -eq 0) {$canConnectToGitHub = $true} 
 else {$canConnectToGitHub = $false}
 
