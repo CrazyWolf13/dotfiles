fd7089ca5c3fdcbf30ce446e91b661e91cf358b9
Tobias Meier <tobias.mei@edu.tbz.ch>
2024-09-09 17:01:32 +0200

    fix ohmyposh URL

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ $name= "Tobias" # Change this to your name.
 $githubRepo = "dotfiles" # Change this here if you forked the repository and changed the name.
 $githubBaseURL= "https://raw.githubusercontent.com/$githubUser/$githubRepo/main/pwsh"
 $OhMyPoshConfigFileName = "montys.omp.json" # Filename of the OhMyPosh config file
-$OhMyPoshConfig = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$OhMyPoshConfigFileName" # URL of the OhMyPosh config file, make sure to use the last part of the raw lik, (stands for the filename) in the variable on the line below
+$OhMyPoshConfig = "$githubBaseURL/$OhMyPoshConfigFileName" # URL of the OhMyPosh config file, make sure to use the last part of the raw lik, (stands for the filename) in the variable on the line below
 
 # -----------------------------------------------------------------------------
 
